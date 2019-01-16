c95d537432f37cb1cf5d952346336c7eb04f4238
Idan Izhaki <izhaki.idan@gmail.com>
2019-01-16 13:31:00 +0200

    include build folder for production

---
+++ build/precache-manifest.c727c24b343c61e9dbf3bcd61edb5bae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f7cc97cb1b791296f0a",
+    "url": "/static/css/main.40325a04.chunk.css"
+  },
+  {
+    "revision": "8f7cc97cb1b791296f0a",
+    "url": "/static/js/main.8f7cc97c.chunk.js"
+  },
+  {
+    "revision": "f38617a23831a9d27202",
+    "url": "/static/js/1.f38617a2.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ee064136a26ef21a2e73739a6e287f9e",
+    "url": "/index.html"
+  }
+];
